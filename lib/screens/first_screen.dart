4c9d95dd213f9835ef39e0c809b270af075bd55b
snehaljha <snehaljha08@gmail.com>
2021-02-09 21:39:36 +0530

    UI skeleton

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lists/screens/second_screen.dart';
+import 'package:path/path.dart';
 
 class firstScreen extends StatelessWidget {
   @override
@@ -8,58 +9,58 @@ class firstScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(
           "Lists",
-          style: TextStyle(
-            color: Color(0xFFC9C9C9)
-          ),
+          style: TextStyle(color: Color(0xFFC9C9C9)),
         ),
         backgroundColor: Colors.black87,
       ),
       body: Material(
-
         color: Colors.black,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             InkWell(
-              onTap: (){goTo('alphabets', context);},
-              child:
-            Container(
-              padding: EdgeInsets.all(30.0),
-              color: Color(0xff8b8b8b),
-              alignment: Alignment.centerLeft,
-              margin: EdgeInsets.only(bottom: 10.0),
-              child: Text(
-                "Alphabets",
-                style: TextStyle(
-                  color: Colors.white70,
-                ),
-              ),
-
-            )),
+                onTap: () {
+                  goTo('alphabets', context);
+                },
+                child: Container(
+                  padding: EdgeInsets.all(30.0),
+                  color: Color(0xff8b8b8b),
+                  alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.only(bottom: 10.0),
+                  child: Text(
+                    "Alphabets",
+                    style: TextStyle(
+                      color: Colors.white70,
+                    ),
+                  ),
+                )),
             InkWell(
-              onTap: (){goTo('numbers', context);},
-              child:
-            Container(
-              padding: EdgeInsets.all(30.0),
-              color: Color(0xff8b8b8b),
-              alignment: Alignment.centerLeft,
-              margin: EdgeInsets.only(bottom: 10.0),
-              child: Text(
-                "Numbers",
-                style: TextStyle(
-                  color: Colors.white70,
-                ),
-              ),
-            )
-            )
+                onTap: () {
+                  goTo('numbers', context);
+                },
+                child: Container(
+                  padding: EdgeInsets.all(30.0),
+                  color: Color(0xff8b8b8b),
+                  alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.only(bottom: 10.0),
+                  child: Text(
+                    "Numbers",
+                    style: TextStyle(
+                      color: Colors.white70,
+                    ),
+                  ),
+                ))
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
     );
   }
-
 }
 
 goTo(String str, context) {
-  Navigator.of(context).push(MaterialPageRoute(builder: (context) => secondScreen(str)));
+  Navigator.of(context)
+      .push(MaterialPageRoute(builder: (context) => secondScreen(str)));
 }
--- lib/screens/second_screen.dart
+++ lib/screens/second_screen.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 
-var alphabets = ['Alpha', 'bravo', 'charlie', 'delta', 'echo', 'foxtrot', 'golf', 'hotel', 'india', 'juliet'];
-var numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+var alphabets = [
+  'Alpha',
+  'bravo',
+  'charlie',
+  'delta',
+  'echo',
+  'foxtrot',
+  'golf',
+  'hotel',
+  'india',
+  'juliet'
+];
+var numbers = [
+  'one',
+  'two',
+  'three',
+  'four',
+  'five',
+  'six',
+  'seven',
+  'eight',
+  'nine',
+  'ten'
+];
 
 class secondScreen extends StatelessWidget {
   String list_name = "alphabet";
@@ -15,35 +37,68 @@ class secondScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(
           list_name,
-          style: TextStyle(
-              color: Color(0xFFC9C9C9)
-          ),
+          style: TextStyle(color: Color(0xFFC9C9C9)),
         ),
         backgroundColor: Colors.black87,
       ),
       body: Material(
         color: Colors.black,
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: getItems(list_name),
-          ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: 50.0,
+              color: Colors.red,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    height: 30.0,
+                    width: getTFWidth(context),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Enter Something',
+                      ),
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  ),
+                  RaisedButton(
+                    child: Text('Add'),
+                    splashColor: Colors.white30,
+                    onPressed: () {
+                      print('button pressed');
+                    },
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: getItems(list_name),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
-
 }
 
 getItems(String list_name) {
-  if(list_name == 'alphabets')
-    return generateItems(alphabets);
+  if (list_name == 'alphabets') return generateItems(alphabets);
   return generateItems(numbers);
 }
 
 generateItems(List list) {
   List items = List<Widget>();
-  for(String name in list) {
+  for (String name in list) {
     Widget widget = new Container(
       padding: EdgeInsets.all(30.0),
       margin: EdgeInsets.only(bottom: 10.0),
@@ -59,4 +114,9 @@ generateItems(List list) {
     items.add(widget);
   }
   return items;
+}
+
+getTFWidth(context) {
+  double w = MediaQuery.of(context).size.width;
+  return w - 100;
 }
